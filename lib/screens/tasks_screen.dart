75782e9d3fba00a4c5a5dec2953f5ca153cca820
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-24 09:10:23 +0100

    Styled widgets

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -14,8 +14,26 @@ class TasksScreen extends StatelessWidget {
         ),
         child: Column(
           children: <Widget>[
-            Icon(Icons.list),
-            Text('Todoey'),
+            CircleAvatar(
+              child: Icon(
+                Icons.list,
+                size: 30.0,
+                color: Colors.lightBlueAccent,
+              ),
+              backgroundColor: Colors.white,
+              radius: 30.0,
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Todoey',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 50.0,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
           ],
         ),
       ),
