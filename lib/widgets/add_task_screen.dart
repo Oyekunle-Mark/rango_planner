4b8a269216cac697dede7e882c74de76b7e37d2c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-24 09:59:01 +0100

    Styled and curved the inner container

--- lib/widgets/add_task_screen.dart
+++ lib/widgets/add_task_screen.dart
@@ -3,6 +3,17 @@ import 'package:flutter/material.dart';
 class AddTaskScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Color(0xff757575),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topRight: Radius.circular(20.0),
+            topLeft: Radius.circular(20.0),
+          ),
+        ),
+      ),
+    );
   }
 }
