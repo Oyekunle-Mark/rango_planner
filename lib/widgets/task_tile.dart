77b1fdd1d73c1d70ca2390d6bb9b54f80add9cd2
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-24 10:43:45 +0100

    Used global state to update text and check box

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -8,30 +8,41 @@ class TaskTile extends StatefulWidget {
 class _TaskTileState extends State<TaskTile> {
   bool isChecked = false;
 
+  void checkBoxCallback(bool checkBoxState) {
+    setState(() {
+      isChecked = checkBoxState;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('This is a task'),
-      trailing: TaskCheckBox(),
+      title: Text(
+        'This is a task',
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: TaskCheckBox(
+        isChecked,
+        checkBoxCallback,
+      ),
     );
   }
 }
 
 class TaskCheckBox extends StatelessWidget {
   final bool checkBoxState;
+  final Function toggleState;
 
-  TaskCheckBox(this.checkBoxState);
+  TaskCheckBox(this.checkBoxState, this.toggleState);
 
   @override
   Widget build(BuildContext context) {
     return Checkbox(
       activeColor: Colors.lightBlueAccent,
       value: checkBoxState,
-      onChanged: (newValue) {
-//        setState(() {
-//          checkBoxState = newValue;
-//        });
-      },
+      onChanged: toggleState,
     );
   }
 }
