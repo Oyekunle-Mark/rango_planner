9eb0b7addee8db83950dbb84a7926c97d7cc8f4f
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-24 11:27:41 +0100

    Lifted state up

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:todoey/widgets/tasks_list.dart';
 import 'package:todoey/widgets/add_task_screen.dart';
+import 'package:todoey/model/task.dart';
 
-class TasksScreen extends StatelessWidget {
+class TasksScreen extends StatefulWidget {
+  @override
+  _TasksScreenState createState() => _TasksScreenState();
+}
+
+class _TasksScreenState extends State<TasksScreen> {
+  List<Task> tasks = [
+    Task(name: 'Build compiler'),
+    Task(name: 'Play some console'),
+    Task(name: 'Finish the book'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -80,7 +92,7 @@ class TasksScreen extends StatelessWidget {
                     topRight: Radius.circular(20.0),
                   ),
                 ),
-                child: TasksList()),
+                child: TasksList(tasks)),
           ),
         ],
       ),
--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -3,31 +3,29 @@ import 'package:todoey/widgets/task_tile.dart';
 import 'package:todoey/model/task.dart';
 
 class TasksList extends StatefulWidget {
+  final List<Task> tasks;
+
+  TasksList(this.tasks);
+
   @override
   _TasksListState createState() => _TasksListState();
 }
 
 class _TasksListState extends State<TasksList> {
-  List<Task> tasks = [
-    Task(name: 'Build compiler'),
-    Task(name: 'Play some console'),
-    Task(name: 'Finish the book'),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemBuilder: (context, index) {
         return TaskTile(
-            taskTitle: tasks[index].name,
-            isChecked: tasks[index].isDone,
+            taskTitle: widget.tasks[index].name,
+            isChecked: widget.tasks[index].isDone,
             checkBoxCallback: (checkBoxState) {
               setState(() {
-                tasks[index].toggleDone();
+                widget.tasks[index].toggleDone();
               });
             });
       },
-      itemCount: tasks.length,
+      itemCount: widget.tasks.length,
     );
   }
 }
