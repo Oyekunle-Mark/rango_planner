9e62be1af7cddec68f6c3d41d95ab99ed1846538
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-24 17:57:44 +0100

    Made tasks private

--- lib/model/task_data.dart
+++ lib/model/task_data.dart
@@ -2,15 +2,15 @@ import 'package:flutter/cupertino.dart';
 import 'package:todoey/model/task.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: 'Build compiler'),
     Task(name: 'Play some console'),
     Task(name: 'Finish the book'),
   ];
 
-  int get taskCount => tasks.length;
+  int get taskCount => _tasks.length;
   void addTask(String taskName) {
-    tasks.add(Task(name: taskName));
+    _tasks.add(Task(name: taskName));
     notifyListeners();
   }
 }
