6a3985442f9fb3f39bdf7f5ae763471633faf2b6
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-24 10:06:10 +0100

    Added the bottom sheet components

--- lib/widgets/add_task_screen.dart
+++ lib/widgets/add_task_screen.dart
@@ -13,6 +13,33 @@ class AddTaskScreen extends StatelessWidget {
             topLeft: Radius.circular(20.0),
           ),
         ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30.0,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            TextField(
+              autofocus: true,
+              textAlign: TextAlign.center,
+            ),
+            FlatButton(
+              child: Text(
+                'Add',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              color: Colors.lightBlueAccent,
+              onPressed: () {},
+            ),
+          ],
+        ),
       ),
     );
   }
