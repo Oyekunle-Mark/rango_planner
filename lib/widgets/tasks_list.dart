759970d0f7b81bb5953d09a8ef25e840ba1c16fa
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-24 18:15:54 +0100

    Updated task from the consumer

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -10,11 +10,13 @@ class TasksList extends StatelessWidget {
       builder: (context, taskData, child) {
         return ListView.builder(
           itemBuilder: (context, index) {
+            final task = taskData.tasks[index];
+
             return TaskTile(
-                taskTitle: taskData.tasks[index].name,
-                isChecked: taskData.tasks[index].isDone,
+                taskTitle: task.name,
+                isChecked: task.isDone,
                 checkBoxCallback: (checkBoxState) {
-//
+                  taskData.updateTask(task);
                 });
           },
           itemCount: taskData.taskCount,
